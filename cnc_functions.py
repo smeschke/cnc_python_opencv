53f961d0ff2c146eb862bfd4725b25f99a353576
Stephen Meschke <stephenmeschke@gmail.com>
2019-12-01 09:50:31 -0800

    Update cnc_functions.py

--- cnc_functions.py
+++ cnc_functions.py
@@ -2,6 +2,67 @@ import cv2
 import numpy as np
 import math
 
+def get_finish_pass(img, cutter_size, clear_depth, cut_depth, pixels_per_inch, feedSpeed, jogSpeed, plungeSpeed):
+        
+    # Find the contours in the image
+    contours = find_contours(img)
+    print('eroding image')
+    # Draw the contours by the cutter size to erode the image
+    cv2.drawContours(img, contours, -1, 255, cutter_size)
+    cv2.imshow('img', cv2.resize(img, (567,567)))
+    cv2.waitKey(1000)
+
+
+    # Make a list for the contour positions
+    contourPositions = []
+    # Find the center of the part - there should only be one contour
+    contours, heir = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    print(len(contours))
+    # Iterate through the contours in the image
+    for contour, h in zip(contours, heir[0]):
+        # Convert the contour to the list
+        contour_list = contour_to_list(contour)
+        # Create a list for the current contour positions
+        current_contour_positions = []
+        # Add the first contour point to the list
+        current_contour_positions.append(contour_list[0])
+        for i in range(len(contour_list)-1):
+            a = contour_list[i]
+            b = contour_list[i-1]
+            current_contour_positions.append(b)
+            cv2.line(img, a, b, 123, cutter_size)
+            cv2.imshow('img', cv2.resize(img, (567,567)))
+            k = cv2.waitKey(1)
+            if k == 27: break
+        # Re-add the first point to the list so that it will complete the shape
+        current_contour_positions.append(current_contour_positions[0])
+        # Save the current contour positions to the list
+        contourPositions.append(current_contour_positions)
+    g_code = contour_to_gcode(contourPositions, pixels_per_inch, clear_depth, cut_depth, jogSpeed, feedSpeed, plungeSpeed)
+    return g_code
+
+# Function that converts lists of contours points to g-code
+def contour_to_gcode(contourPositions, pixels_per_inch, clear_depth, cut_depth, jogSpeed, feedSpeed, plungeSpeed):
+    g_code = ''
+    for points in contourPositions:
+        print('writing contour')
+        # Go to the first point
+        g_code += "G1 F" +  jogSpeed + "\n"
+        g_code += "X" + str(points[0][0]/pixels_per_inch) + "Y" + str(points[0][1]/pixels_per_inch) + "\n"
+        # Lower the cutter
+        g_code += "G1 F" +  plungeSpeed + "\n"
+        g_code +=  "Z" + str(cut_depth/pixels_per_inch) + "\n"
+        g_code += "G1 F" +  feedSpeed + "\n"
+        for point in points:
+            g_code += "X" + str(point[0]/pixels_per_inch) + "Y" + str(point[1]/pixels_per_inch) + "\n"
+        # Go to the first point again to complete the contour
+        g_code += "X" + str(points[0][0]/pixels_per_inch) + "Y" + str(points[0][1]/pixels_per_inch) + "\n"
+        # Retract the cutter
+        g_code += "Z" + str(clear_depth/pixels_per_inch) + "\n"
+        g_code += "G1 F" +  jogSpeed + "\n"
+    # return the text g-code    
+    return g_code
+
 def buffer(img, cutter_size, buffer_factor):
     h,w = img.shape
     b = int(cutter_size* 1.25)
@@ -29,20 +90,6 @@ def erode_contour(contour, width, height, kernelSize, iterations):
     img = cv2.erode(img, kernel, iterations = iterations)
     contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     return contours[0]
-
-def expand_part(img, cutterSize, pixelsToInch):
-    h,w = img.shape
-    contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for x in range(w):
-        for y in range(y):
-            px = img[y,x]
-            if px == 0:
-                minDist = cutterSize * 2
-                for contour in contours:
-                    dist = abs(cv2.pointPolygonTest(contour, (x,y), True))
-                    if dist < minDist: minDist = dist
-                if minDist < cutterSize/2: img[y,x] = 123
-    return img
 
 def g_code_header(jog_speed):
     g_code = "%" + "\n"
@@ -133,4 +180,12 @@ def carve_region_contour_to_points(img, contour, cutter_size, stepover, w):
     path.append(points[-1])
     path.append(points[0])
     return path
+
+def find_contours(img):
+    try: x,y = img.shape
+    except: print('must use grayscale image in cnc_functions.find_contours(img)')
+    contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+
     
